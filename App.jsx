fe660fe5d943a8f59fd06d1945009c58b206e654
Joan Corporan <joan.corporan.h@gmail.com>
2023-12-06 08:53:22 -0300

    firts commit

---
+++ App.jsx
@@ -0,0 +1,46 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+const CAT_ENDPOINT_RANDOM_FACT ="https://catfact.ninja/fact"
+const IMAGE='https://catfact.com/cat/says/${threeFirstWords}?size=50&color=red&json=true'
+function App (){
+    const [fact,setFact]=useState()
+    const [word,setWord]=useState()
+    /* useEffect(()=>{
+        fetch(CAT_ENDPOINT_RANDOM_FACT)
+        .then(res=> res.json())
+        .then(data=> setFact(data.fact))
+    },[]) */ 
+    useEffect(()=>{
+        const getFactRandom = async ()=>{
+            try{
+            const {data} = await axios.get(CAT_ENDPOINT_RANDOM_FACT)
+            
+            setFact(data.fact);
+            console.log(fact)
+            
+            /* const firstWord = fact.split(' ').slice(0,3).join(' ')
+            console.log(firstWord) */
+            const threeFirstWords = fact.split(' ').slice(0,3).join(' ')
+            console.log(threeFirstWords)
+            setWord(threeFirstWords)
+            }catch(error){
+                console.log("error",error)
+            }
+            
+            
+          /*  const {dataImg} = await axios.get(`https://catfact.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`) 
+           console.log(dataImg) */
+
+        }
+        getFactRandom()
+    },[])
+return(
+    <main>
+        <h1>App de gatos</h1>
+        {fact && <p>{fact}</p> }
+        {word && <h2>{word}</h2>}
+    </main>
+)
+}
+export default App
